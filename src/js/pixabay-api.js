9992e6e25a236db836b7c32f0dbdcd36ce2c8aec
Anna Velychko <162054651+Annvel126V@users.noreply.github.com>
2024-08-01 20:16:46 +0200

    fix params

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -2,15 +2,15 @@
 
 import axios from "axios";
 
-const BASE_URL = "https://pixabay.com/api/"; 
+const URL = "https://pixabay.com/api/"; 
 const API_KEY = "45110935-91aa782eb5f23a60393d7a38c";
 
 export const searchImages = async (query, page = 1, perPage = 15) => {
     
     try {
-        const response = await axios.get(BASE_URL, {
+        const response = await axios.get(URL, {
   
-            parms: {
+            params: {
                 key: API_KEY,
                 q: query,
                 image_type: 'photo',
